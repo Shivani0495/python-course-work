2393f93613f609ec2117bb4ac8f7b93e61aa5fe7
Shivani0495 <kandukurishivani260@gmail.com>
2025-04-16 15:42:14 +0530

    16-04-2025 class work

---
+++ codeplay/prime.py
@@ -0,0 +1,10 @@
+n=int(input('enter the number:'))
+prime=0
+for i in range(2,(n//2)+1):
+    if n%i==0:
+        prime+=1
+        break
+if prime==0:
+    print(n,'is prime number') 
+else:
+    print(n,'is not prime number')      
